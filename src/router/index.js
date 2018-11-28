3cdc73eddf2330892f6385122b4d6a8a2117f978
Huy Bui <buiquanghuy.it@gmail.com>
2018-11-29 00:15:54 +0700

    Add admin child view

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from '@/pages/Home'
 import Admin from '@/pages/Admin'
 import Cart from '@/pages/Cart'
+
+//admin child
+import Index from '@/pages/admin/Index'
+import New from '@/pages/admin/New'
+import Products from '@/pages/admin/Product'
+import Edit from '@/pages/admin/Edit'
 
 Vue.use(Router)
 
@@ -16,7 +23,25 @@ export default new Router({
     {
       path: '/Admin',
       name: 'Admin',
-      component: Admin
+      component: Index,
+       // Child routes
+       children: [
+        {
+          path: 'new',
+          name: 'New',
+          component: New
+        },
+        {
+          path: '',
+          name: 'Products',
+          component: Products
+        },
+        {
+          path: 'edit/:id',
+          name: 'Edit',
+          component: Edit
+        }
+      ]
     },
     {
       path: '/Cart',
